7dfc1a204e846fb62af76dbb1965cc935dc5e965
Matthew Campbell <mjmcee@gmail.com>
2017-08-28 18:56:28 -0400

    updaing leaflet

--- package.js
+++ package.js
@@ -1,14 +1,14 @@
 Package.describe({
-  name: "bevanhunt:leaflet",
-  summary: "leaflet - mobile-friendly maps.",
-  git: "https://github.com/bevanhunt/meteor-leaflet.git",
+  name: "maka:leaflet",
+  summary: "Just like bevanhunts, but trying to get rotation working.",
+  git: "https://github.com/maka-io//meteor-leaflet.git",
   author: "Bevan Hunt <bevan@bevanhunt.com> (http://bevanhunt.com)",
-  version: "3.1.1",
+  version: "3.1.3",
   license: "MIT"
 });
 
 Npm.depends({
-  "leaflet": "1.0.3",
+  "leaflet": "1.2.0",
   "leaflet.markercluster": "1.0.2",
   "leaflet-providers": "1.1.16",
   "leaflet-spin": "1.1.0",
@@ -17,7 +17,7 @@ Npm.depends({
 });
 
 Package.onUse(function (api) {
-  api.versionsFrom('1.2');
+  api.versionsFrom('1.5');
   api.addFiles([
     '.npm/package/node_modules/leaflet/dist/leaflet-src.js',
     '.npm/package/node_modules/leaflet-providers/leaflet-providers.js',
@@ -26,6 +26,7 @@ Package.onUse(function (api) {
     '.npm/package/node_modules/leaflet.markercluster/dist/MarkerCluster.Default.css',
     '.npm/package/node_modules/spin.js/spin.js',
     '.npm/package/node_modules/leaflet-spin/leaflet.spin.js',
+    '.npm/package/node_modules/leaflet-rotatedmarker/leaflet.rotatedMarker.js',
     'styles/leaflet.css',
   ], 'client');
   api.addAssets([
